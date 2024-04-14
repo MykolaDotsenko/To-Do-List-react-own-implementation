eee18c01d3e610971471a0f149a3866e33ca0e7f
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-04-14 08:27:43 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-export const App = () => {
+import { useState } from "react"
+
+function App () {
+  const [todos, setTodos] = useState([
+    {id:'1', title: 'React', completed:false},
+    {id:'2', title: 'JS', completed:false},
+    {id:'3', title: 'CSS', completed:false}
+  ])
+
+const handleDeleteItem = id => {
+  console.log(id);
+  const newTodos = todos.filter(item =>item.id !== id )
+  setTodos(newTodos)
+}
+
+const handleAddTodo = () => {
+  const newTodo = {id:5, title:'TEST TITLE', completed:false}
+  setTodos(prev => [...prev, newTodo])
+}
+
   return (
     <div>
       <form>
         <input type = 'text' />
-        <buttun>Add todo</buttun>
+        <button onClick={handleAddTodo}>Add todo</button>
       </form>
       <ul>
-        <li>Купить хлеб</li>
+        {todos.map(item => (
+        <li key={item.id}>
+          <input type='checkbox'/>
+          <p>{item.title}</p>
+          <button onClick={() =>handleDeleteItem(item.id)}>Delete</button>
+        </li>
+        ))}
       </ul>
     </div>
   )
