5da7dfc6c47f71701674a892102498a02eb00e76
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-04-14 10:03:22 +0300

    finally logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
+import { nanoid } from "nanoid"
 
 function App () {
   const [todos, setTodos] = useState([
-    {id:'1', title: 'React', completed:false},
-    {id:'2', title: 'JS', completed:false},
-    {id:'3', title: 'CSS', completed:false}
   ])
+  const [newTodoTitle, setNewTodoTitle] = useState('')
+
+
+  const handleToggleTodo = id => {
+setTodos(prev => prev.map(item => (item.id === id ? {...item, completed:!item.completed} : item)))
+  }
 
 const handleDeleteItem = id => {
   console.log(id);
@@ -14,20 +18,28 @@ const handleDeleteItem = id => {
 }
 
 const handleAddTodo = () => {
-  const newTodo = {id:5, title:'TEST TITLE', completed:false}
+  const newTodo = {id: nanoid(), title:'TEST TITLE', completed:false}
   setTodos(prev => [...prev, newTodo])
+}
+const handleSubmit = (e) => {
+e.preventDefault()
+console.log('Form was submitted');
+const newTodo = {id: nanoid(), title:newTodoTitle, completed:false}
+setTodos(prev => [...prev, newTodo])
+setNewTodoTitle('')
 }
 
   return (
     <div>
-      <form>
-        <input type = 'text' />
-        <button onClick={handleAddTodo}>Add todo</button>
+      <form onSubmit={handleSubmit}>
+        <input value={newTodoTitle} onChange={(e) => setNewTodoTitle(e.target.value)} type = 'text' />
+        <button>Add todo</button>
       </form>
+      <button onClick={handleAddTodo}>Add</button>
       <ul>
         {todos.map(item => (
         <li key={item.id}>
-          <input type='checkbox'/>
+          <input onChange={()=>handleToggleTodo(item.id)}type='checkbox' checked={item.completed}/>
           <p>{item.title}</p>
           <button onClick={() =>handleDeleteItem(item.id)}>Delete</button>
         </li>
